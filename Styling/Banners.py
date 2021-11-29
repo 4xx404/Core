bd028ccc4d9079e35abe95b51781f8c06b4436ac
4xx404 <45526353+4xx404@users.noreply.github.com>
2021-11-29 15:07:02 +0000

    Create Banners.py

---
+++ Styling/Banners.py
@@ -0,0 +1,11 @@
+import emojis
+from .Colors import bc
+
+class sd:
+	iBan: str = emojis.encode(f" :information_source:{bc.BC} ")
+	sBan: str = emojis.encode(f" :white_check_mark:{bc.BC} ")
+	eBan: str = emojis.encode(f" :x:{bc.RC} ERROR:{bc.BC}")
+
+	Logo = f'''
+	Enter your Banner here
+	'''

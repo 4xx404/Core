d56d72a8032a2ca802f0968508170447145c5cd1
4xx404 <45526353+4xx404@users.noreply.github.com>
2021-12-03 01:49:34 +0000

    Update FileSystem.py
    Include Type-Casting
    Added ReadFromFile & AppendToFile

--- FileSystem.py
+++ FileSystem.py
@@ -7,11 +7,32 @@ class FileManager:
     def __init__(self):
         self.Error = ErrorHandler()
 
-    def WriteToFile(self, FileName, Content):
-        self.FileName = FileName
-        self.Content = Content
+    def ReadFromFile(self, FileName: str):
+        self.FileName: str = FileName
 
-        self.Response = {
+        self.Response: dict = {
+            "status": False,
+            "data": []
+        }
+
+        try:
+            self.File = open(self.FileName, "r")
+            self.Lines: list = self.File.readlines(self.Content)
+            
+            for self.Line in self.Lines:
+                self.Response["data"].append(self.Line)
+            self.File.close()
+        except Exception:
+            self.Response["data"] = self.Error.Throw("read_from_file_failed", self.FileName)
+        
+        self.Response["status"] = True
+        return self.Response
+
+    def WriteToFile(self, FileName: str, Content: str):
+        self.FileName: str = FileName
+        self.Content: str = Content
+
+        self.Response: dict = {
             "status": False,
             "data": ""
         }
@@ -20,9 +41,31 @@ class FileManager:
             self.File = open(self.FileName, "w")
             self.File.write(self.Content)
             self.File.close()
-            self.Response["status"] = True
-            self.Response["data"] = self.FileName
         except Exception:
             self.Response["data"] = self.Error.Throw("write_to_file_failed", self.FileName)
-        
+
+        self.Response["status"] = True
+        self.Response["data"] = self.FileName        
+        return self.Response
+
+    def AppendToFile(self, FileName: str, Content: list):
+        self.FileName: str = FileName
+        self.Content: list = Content
+
+        self.Response: dict = {
+            "status": False,
+            "data": []
+        }
+
+        try:
+            self.File = open(self.FileName, "a")
+            for self.Data in self.Content:
+                self.File.write(self.Data)
+                self.Response["data"].append(self.Data)
+
+            self.File.close()
+        except Exception:
+            self.Response["data"].append(self.Error.Throw("append_to_file_failed", [self.Data, self.FileName]))
+
+        self.Response["status"] = True
         return self.Response

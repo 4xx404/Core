df2334d9f85a8947b4e5684afd17035e3d5dcc3e
4xx404 <45526353+4xx404@users.noreply.github.com>
2021-12-03 03:18:13 +0000

    Update Request.py
    Added FormatLink method called by CollectAllLinks

--- Request.py
+++ Request.py
@@ -14,9 +14,9 @@ class RequestHandler:
         self.Validator = Validation()
         self.Error = ErrorHandler()
 
+    # Checks for status code 200
     def IsLive(self, Link: str):
         self.Link: str = Link
-        
         try:
             self.Request = requests.get(self.Link, headers=self.Config.Headers)
         except Exception:
@@ -24,10 +24,9 @@ class RequestHandler:
 
         return True
 
+    # Return the page content as bytes by default, set ReturnAsString = True to return as string type
     def GetContent(self, Link: str, ReturnAsString: bool = False):
         self.Link: str = Link
-
-        self.ReturnAsString = ReturnAsString
 
         self.Response = {
             "status": False,
@@ -38,7 +37,7 @@ class RequestHandler:
             if(self.IsLive(self.Link)):
                 self.Request = requests.get(self.Link, headers=self.Config.Headers)
                 
-                if(self.ReturnAsString):
+                if(ReturnAsString):
                     self.Response["status"] = True
                     self.Response["data"] = self.Request.content.decode()
                 else:
@@ -51,6 +50,23 @@ class RequestHandler:
 
         return self.Response
 
+    # Return a usable link or if not usable set to None
+    def FormatLink(self, Link):
+        self.Link = Link
+
+        if(self.Validator.HasProtocol(self.Link)):
+            self.ValidLink = self.Link
+        elif(self.Link.startswith("/") and len(self.Link) > 1):
+            if(self.BaseLink.endswith("/")):
+                self.ValidLink = f"{self.BaseLink[:-1]}{self.Link}"
+            else:
+                self.ValidLink = f"{self.BaseLink}{self.Link}"
+        else:
+            self.ValidLink = None
+
+        return self.ValidLink
+
+    # Collect any "a" html tag's href and format into usable url's
     def CollectAllLinks(self, BaseLink: str):
         self.BaseLink: str = BaseLink
 
@@ -59,24 +75,12 @@ class RequestHandler:
             "data": []
         }
 
-
         if(self.IsLive(self.BaseLink)):
             self.Request = requests.get(self.BaseLink, headers=self.Config.Headers)
             self.Soup = BeautifulSoup(self.Request.content, 'html.parser')
 
             for self.LinkTag in self.Soup.find_all("a", href=True):
-                self.Link = str(self.LinkTag["href"])
-
-                if(self.Link.startswith("http://") or self.Link.startswith("https://")):
-                    self.ValidLink = self.Link
-                elif(self.Link.startswith("/") and len(self.Link) > 1):
-                    if(self.BaseLink.endswith("/")):
-                        self.ValidLink = f"{self.BaseLink[:-1]}{self.Link}"
-                    else:
-                        self.ValidLink = f"{self.BaseLink}{self.Link}"
-                else:
-                    self.ValidLink = None
-
+                self.ValidLink = self.FormatLink(self.BaseLink, str(self.LinkTag["href"]))
                 if(not self.ValidLink in self.Response["data"] and self.ValidLink != None):
                     self.Response["status"] = True
                     self.Response["data"].append(self.ValidLink)
